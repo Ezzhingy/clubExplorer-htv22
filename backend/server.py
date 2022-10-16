436fca629d88692c0e17f56e78747d679300049d
Howass <geekdaoxin@gmail.com>
2022-10-16 08:48:57 -0400

    add event

--- backend/server.py
+++ backend/server.py
@@ -38,6 +38,50 @@ def list():
     for row in rows:
         data.append([x for x in row])
     return data
-  
+
+
+@app.route('/api/addrec_event', methods=['POST'])
+def addrec_event():
+    if request.method == 'POST':
+        try:
+            name = request.form['input-name']
+            event_name = request.form['input-event-name']
+            location = request.form['same-location']
+            time = request.form['same-time']
+            description = request.form['input-description']
+            cultural= request.form['input-cultural-event']
+            price = request.form['input-price']
+
+            with sql.connect('database.db') as con:
+                cur = con.cursor()
+
+                cur.execute("INSERT INTO add_event (Clubname,Event_name,location,time,descrip,culture,price)VALUES(?,?,?,?,?,?,?)",
+                            (name, event_name,location,time,description,cultural,price))
+
+                con.commit()
+
+            return redirect("/browse")
+        except:
+            con.rollback()
+        finally:
+            con.close()
+
+
+
+
+@app.route('/api/list_event')
+def list():
+    name = request.form['input-name']
+    con = sql.connect("database.db")
+    con.row_factory = sql.Row
+    cur = con.cursor()
+    cur.execute("SELECT * FROM add_event WHERE Clubname=(name)VALUES(?)",name)
+
+    rows = cur.fetchall()
+    data = []
+    for row in rows:
+        data.append([x for x in row])
+    return data
+
 if __name__ == '__main__':
     app.run(debug=True)
